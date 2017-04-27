75f1868247998f3788adbbeeb4a4f7639b3b3e8d
Santosh Bharadwaj <rsb.bth@gmail.com>
2017-04-27 20:17:46 +0200

    Initial Commit

---
+++ src/main/java/se/sics/tac/aw/App.java
@@ -0,0 +1,13 @@
+package se.sics.tac.aw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
